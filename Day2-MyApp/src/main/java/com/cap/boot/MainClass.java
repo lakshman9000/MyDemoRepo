41656b6bd01790c8e673ef3b689f45ba2f0a9cde
lanaik <lanaik@DIN76000820.corp.capgemini.com>
2017-11-15 12:10:30 +0530

    branch2 changes only

--- Day2-MyApp/src/main/java/com/cap/boot/MainClass.java
+++ Day2-MyApp/src/main/java/com/cap/boot/MainClass.java
@@ -14,6 +14,8 @@ public class MainClass {
 		System.out.println("Good morning!");
 		System.out.println("Greetings !");
 		System.out.println("this statement is only in branch-1");
+		
+		System.out.println("this statement is only in branch-2");
 	}
 
 }
